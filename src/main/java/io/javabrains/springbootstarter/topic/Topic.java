5ea50a94ac10d532756735e387ba63930ef8abf8
Anunai Ishan <ishananunai@gmail.com>
2020-04-15 11:43:23 -0400

    Introduce Lombok

--- src/main/java/io/javabrains/springbootstarter/topic/Topic.java
+++ src/main/java/io/javabrains/springbootstarter/topic/Topic.java
@@ -1,14 +1,17 @@
 package io.javabrains.springbootstarter.topic;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
 public class Topic {
 
-    private String id;
-    private String name;
-    private String description;
-
-    public Topic() {
-
-    }
+    @Getter @Setter private String id;
+    @Getter @Setter private String name;
+    @Getter @Setter private String description;
 
     public Topic(String id, String name, String description) {
         super();
@@ -17,27 +20,4 @@ public class Topic {
         this.description = description;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
